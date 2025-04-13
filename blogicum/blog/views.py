0b91779998543ec4a6600c55d5c05acdcbc2e056
Бехруз Холов <baha2121mers0824@gmail.com>
2025-04-13 19:18:10 +0300

    Выполнено финальное задание спринта попытка 36

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -9,7 +9,6 @@ from blog.utils import posts_pagination, query_post
 
 
 def index(request):
-    post_list = Post.objects.order_by('-pub_date')
     return render(
         request,
         'blog/index.html',
