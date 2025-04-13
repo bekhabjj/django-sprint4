0b2c6f2aca53395051c11e9cd4e2e52e20421ad1
Бехруз Холов <baha2121mers0824@gmail.com>
2025-04-14 01:08:11 +0300

    исправления 12

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -101,7 +101,7 @@ def profile(request, username=None):
     posts = get_posts(
         user.posts.all(),
         apply_filters=(
-            not request.user.is_authenticated 
+            not request.user.is_authenticated
             or request.user != user
         ),
         apply_default_ordering=False
