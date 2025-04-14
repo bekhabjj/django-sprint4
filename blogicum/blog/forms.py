c25b2d39bd4dbb016df94cd9d923986ddb42d44d
Бехруз Холов <baha2121mers0824@gmail.com>
2025-04-15 02:25:40 +0300

    исправления 35

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -20,4 +20,4 @@ class PostForm(forms.ModelForm):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name", "email")
+        fields = ['username', 'first_name', 'last_name', 'email']
