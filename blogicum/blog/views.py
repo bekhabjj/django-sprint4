2d40a3a546215b8f067a50d4ce722bff8833584c
Бехруз Холов <baha2121mers0824@gmail.com>
2025-04-14 23:39:35 +0300

    исправления 22

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 
 from blog.forms import CommentForm, PostForm, ProfileForm
@@ -11,8 +10,8 @@ from blog.utils import get_posts, posts_pagination
 def index(request):
     return render(
         request,
-        'blog/index.html',
-        {'page_obj': posts_pagination(request, get_posts())},
+        "blog/index.html",
+        {"page_obj": posts_pagination(request, get_posts())},
     )
 
 
@@ -21,24 +20,24 @@ def category_posts(request, category_slug):
         Category.objects.filter(is_published=True),
         slug=category_slug,
     )
-    return render(request, 'blog/category.html', {
-        'category': category,
-        'page_obj': posts_pagination(
+    return render(request, "blog/category.html", {
+        "category": category,
+        "page_obj": posts_pagination(
             request,
-            get_posts(category.posts.all()),
+            get_posts(category.posts.all())
         ),
     })
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(
-        get_posts(Post.objects.select_related('author', 'category')),
+        Post.objects.select_related("author", "category"),
         pk=post_id,
     )
-    return render(request, 'blog/detail.html', {
-        'post': post,
-        'form': CommentForm(),
-        'comments': post.comments,
+    return render(request, "blog/detail.html", {
+        "post": post,
+        "form": CommentForm(),
+        "comments": post.comments,
     })
 
 
@@ -46,18 +45,18 @@ def post_detail(request, post_id):
 def create_post(request):
     form = PostForm(request.POST or None, files=request.FILES or None)
     if not form.is_valid():
-        return render(request, 'blog/create.html', {'form': form})
+        return render(request, "blog/create.html", {"form": form})
     post = form.save(commit=False)
     post.author = request.user
     post.save()
-    return redirect('blog:profile', username=request.user.username)
+    return redirect("blog:profile", username=request.user.username)
 
 
 @login_required
 def edit_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     if request.user != post.author:
-        return redirect('blog:post_detail', post_id)
+        return redirect("blog:post_detail", post_id)
     form = PostForm(
         request.POST or None,
         files=request.FILES or None,
@@ -65,19 +64,19 @@ def edit_post(request, post_id):
     )
     if form.is_valid():
         form.save()
-        return redirect('blog:post_detail', post_id)
-    return render(request, 'blog/create.html', {'form': form})
+        return redirect("blog:post_detail", post_id)
+    return render(request, "blog/create.html", {"form": form})
 
 
 @login_required
 def delete_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     if request.user != post.author:
-        return redirect('blog:post_detail', post_id)
-    if request.method == 'POST':
+        return redirect("blog:post_detail", post_id)
+    if request.method == "POST":
         post.delete()
-        return redirect('blog:index')
-    return render(request, 'blog/create.html', {'form': PostForm(instance=post)})
+        return redirect("blog:index")
+    return render(request, "blog/create.html", {"form": PostForm(instance=post)})
 
 
 def profile(request, username=None):
@@ -89,9 +88,9 @@ def profile(request, username=None):
         author.posts.all(),
         apply_filters=(request.user != author),
     )
-    return render(request, 'blog/profile.html', {
-        'profile': author,
-        'page_obj': posts_pagination(request, posts),
+    return render(request, "blog/profile.html", {
+        "profile": author,
+        "page_obj": posts_pagination(request, posts),
     })
 
 
@@ -100,8 +99,11 @@ def edit_profile(request):
     form = ProfileForm(request.POST or None, instance=request.user)
     if form.is_valid():
         form.save()
-        return redirect('blog:profile', username=request.user.username)
-    return render(request, 'blog/user.html', {'form': form})
+        return redirect(
+            "blog:profile",
+            username=request.user.username,
+        )
+    return render(request, "blog/user.html", {"form": form})
 
 
 @login_required
@@ -112,21 +114,21 @@ def add_comment(request, post_id):
         comment.post = get_object_or_404(Post, id=post_id)
         comment.author = request.user
         comment.save()
-    return redirect('blog:post_detail', post_id)
+    return redirect("blog:post_detail", post_id)
 
 
 @login_required
 def edit_comment(request, post_id, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
     if request.user != comment.author:
-        return redirect('blog:post_detail', post_id)
+        return redirect("blog:post_detail", post_id)
     form = CommentForm(request.POST or None, instance=comment)
     if form.is_valid():
         form.save()
-        return redirect('blog:post_detail', post_id)
-    return render(request, 'blog/comment.html', {
-        'form': form,
-        'comment': comment,
+        return redirect("blog:post_detail", post_id)
+    return render(request, "blog/comment.html", {
+        "form": form,
+        "comment": comment,
     })
 
 
@@ -134,8 +136,8 @@ def edit_comment(request, post_id, comment_id):
 def delete_comment(request, post_id, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
     if request.user != comment.author:
-        return redirect('blog:post_detail', post_id)
-    if request.method == 'POST':
+        return redirect("blog:post_detail", post_id)
+    if request.method == "POST":
         comment.delete()
-        return redirect('blog:post_detail', post_id)
-    return render(request, 'blog/comment.html', {'comment': comment})
+        return redirect("blog:post_detail", post_id)
+    return render(request, "blog/comment.html", {"comment": comment})
