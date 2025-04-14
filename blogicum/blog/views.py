0feac8d106e6bac3d4df8e2beaddde36d47b33a5
Бехруз Холов <baha2121mers0824@gmail.com>
2025-04-15 00:37:48 +0300

    исправления 26

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,150 +1,135 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.decorators import login_required
-from django.http import Http404
-from django.shortcuts import get_object_or_404, redirect, render
+from django.db import models
+from django.urls import reverse
 
-from blog.forms import CommentForm, PostForm, ProfileForm
-from blog.models import Category, Comment, Post
-from blog.utils import get_posts, posts_pagination
+from blog.constants import MAX_LENGTH, MAX_WORDS_LENGTH
+
+User = get_user_model()
 
 
-def index(request):
-    return render(
-        request,
-        "blog/index.html",
-        {"page_obj": posts_pagination(request, get_posts())},
+class PublishedBaseModel(models.Model):
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
     )
 
+    class Meta:
+        abstract = True
 
-def category_posts(request, category_slug):
-    category = get_object_or_404(
-        Category.objects.filter(is_published=True),
-        slug=category_slug,
+
+class Category(PublishedBaseModel):
+    title = models.CharField(
+        max_length=MAX_LENGTH,
+        unique=True,
+        verbose_name='Заголовок'
     )
-    return render(request, "blog/category.html", {
-        "category": category,
-        "page_obj": posts_pagination(
-            request,
-            get_posts(category.posts.all())
-        ),
-    })
-
-
-def post_detail(request, post_id):
-    post = get_object_or_404(
-        Post.objects.select_related("author", "category"),
-        pk=post_id,
-    )
-    if not post.is_published and request.user != post.author:
-        raise Http404
-    return render(request, "blog/detail.html", {
-        "post": post,
-        "form": CommentForm(),
-        "comments": post.comments.order_by("created_at"),
-    })
-
-
-@login_required
-def create_post(request):
-    form = PostForm(request.POST or None, files=request.FILES or None)
-    if not form.is_valid():
-        return render(request, "blog/create.html", {"form": form})
-    post = form.save(commit=False)
-    post.author = request.user
-    post.save()
-    return redirect("blog:profile", username=request.user.username)
-
-
-@login_required
-def edit_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.user != post.author:
-        return redirect("blog:post_detail", post_id)
-    form = PostForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=post,
-    )
-    if form.is_valid():
-        form.save()
-        return redirect("blog:post_detail", post_id)
-    return render(request, "blog/create.html", {"form": form})
-
-
-@login_required
-def delete_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.user != post.author:
-        return redirect("blog:post_detail", post_id)
-    if request.method == "POST":
-        post.delete()
-        return redirect("blog:index")
-    return render(
-        request,
-        "blog/create.html",
-        {"form": PostForm(instance=post)},
-    )
-
-
-def profile(request, username=None):
-    author = get_object_or_404(
-        get_user_model(),
-        username=username or request.user.username,
-    )
-    posts = get_posts(
-        author.posts.all(),
-        apply_filters=(request.user != author),
-    )
-    return render(request, "blog/profile.html", {
-        "profile": author,
-        "page_obj": posts_pagination(request, posts),
-    })
-
-
-@login_required
-def edit_profile(request):
-    form = ProfileForm(request.POST or None, instance=request.user)
-    if form.is_valid():
-        form.save()
-        return redirect(
-            "blog:profile",
-            username=request.user.username,
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text=(
+            'Идентификатор страницы для URL; разрешены '
+            'символы латиницы, цифры, дефис и подчёркивание.'
         )
-    return render(request, "blog/user.html", {"form": form})
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title[:MAX_WORDS_LENGTH]
 
 
-@login_required
-def add_comment(request, post_id):
-    form = CommentForm(request.POST or None)
-    if form.is_valid():
-        comment = form.save(commit=False)
-        comment.post = get_object_or_404(Post, id=post_id)
-        comment.author = request.user
-        comment.save()
-    return redirect("blog:post_detail", post_id)
+class Location(PublishedBaseModel):
+    name = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Название места'
+    )
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name[:MAX_WORDS_LENGTH]
 
 
-@login_required
-def edit_comment(request, post_id, comment_id):
-    comment = get_object_or_404(Comment, id=comment_id)
-    if request.user != comment.author:
-        return redirect("blog:post_detail", post_id)
-    form = CommentForm(request.POST or None, instance=comment)
-    if form.is_valid():
-        form.save()
-        return redirect("blog:post_detail", post_id)
-    return render(request, "blog/comment.html", {
-        "form": form,
-        "comment": comment,
-    })
+class Post(PublishedBaseModel):
+    title = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Заголовок'
+    )
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text=(
+            'Если установить дату и время в будущем — '
+            'можно делать отложенные публикации.'
+        )
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации'
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Местоположение'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Категория'
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='posts_images',
+        blank=True
+    )
+
+    class Meta:
+        default_related_name = 'posts'
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title[:MAX_WORDS_LENGTH]
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
 
 
-@login_required
-def delete_comment(request, post_id, comment_id):
-    comment = get_object_or_404(Comment, id=comment_id)
-    if request.user != comment.author:
-        return redirect("blog:post_detail", post_id)
-    if request.method == "POST":
-        comment.delete()
-        return redirect("blog:post_detail", post_id)
-    return render(request, "blog/comment.html", {"comment": comment})
+class Comment(PublishedBaseModel):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        verbose_name='Пост'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    text = models.TextField('Текст комментария')
+
+    class Meta:
+        default_related_name = 'comments'
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f'Комментарий {self.text[:MAX_WORDS_LENGTH]} от {self.author}'
