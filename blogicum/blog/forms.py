ce464685e5d34f87f59b9b886dc8ceb922a35e25
Бехруз Холов <baha2121mers0824@gmail.com>
2025-04-14 16:48:16 +0300

    исправления 26

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from blog.models import Comment, Post, User
+from blog.models import Comment, Post
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class CommentForm(forms.ModelForm):
--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 from blog.constants import MAX_LENGTH, MAX_WORDS_LENGTH
 
@@ -101,3 +102,37 @@ class Post(PublishedBaseModel):
     class Meta:
         default_related_name = 'posts'
         verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title[:MAX_WORDS_LENGTH]
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
+
+
+class Comment(PublishedBaseModel):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        verbose_name='Пост'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    text = models.TextField('Текст комментария')
+
+    class Meta:
+        default_related_name = 'comments'
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f'Комментарий {self.text[:MAX_WORDS_LENGTH]} от {self.author}'
+
+
+__all__ = ['PublishedBaseModel', 'Category', 'Location', 'Post', 'Comment']
