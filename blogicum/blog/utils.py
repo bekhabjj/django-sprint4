52597a492a33f911816048c10939b4c9f272a9d5
Бехруз Холов <baha2121mers0824@gmail.com>
2025-04-16 01:49:49 +0300

    исправления 42

--- blogicum/blog/utils.py
+++ blogicum/blog/utils.py
@@ -1,30 +1,25 @@
-from django.core.paginator import Paginator
-from django.db.models import Count
-from django.utils import timezone
+from django.core.paginator import Paginator 
+from django.db.models import Count 
+from django.utils import timezone 
+from blog.models import Post 
 
-from blog.models import Post
+def posts_pagination(request, queryset, per_page=10): 
+    return Paginator(queryset, per_page).get_page(request.GET.get('page')) 
 
-
-def posts_pagination(request, queryset, per_page=10):
-    return Paginator(queryset, per_page).get_page(request.GET.get('page'))
-
-
-def get_posts(
-    posts=Post.objects.all(),
-    apply_filters: bool = True,
-    with_comments_count: bool = True,
-    use_select_related: bool = True,
-    apply_default_ordering: bool = True
-):
-    if use_select_related:
-        posts = posts.select_related('author', 'location', 'category')
-    if apply_filters:
-        posts = posts.filter(
-            is_published=True,
-            pub_date__lt=timezone.now(),
-            category__is_published=True
-        )
-    if with_comments_count:
-        posts = posts.annotate(comment_count=Count('comments'))
-    posts = posts.order_by(*Post._meta.ordering)
+def get_posts( 
+    posts=Post.objects.all(), 
+    apply_filters: bool = True, 
+    with_comments_count: bool = True, 
+    use_select_related: bool = True, 
+): 
+    if use_select_related: 
+        posts = posts.select_related('author', 'location', 'category') 
+    if apply_filters: 
+        posts = posts.filter( 
+            is_published=True, 
+            pub_date__lt=timezone.now(), 
+            category__is_published=True 
+        ) 
+    if with_comments_count: 
+        posts = posts.annotate(comment_count=Count('comments')) 
     return posts
