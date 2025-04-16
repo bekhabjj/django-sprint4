0e008761174a20b7320722bdc17ec81620a4cdbe
Бехруз Холов <baha2121mers0824@gmail.com>
2025-04-16 13:42:03 +0300

    исправления 78

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,24 +1,23 @@
 from django import forms
-
-from .models import Post, User, Comment
-
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email', 'username')
-
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        exclude = ('author',)
-        widgets = {
-            'pub_date': forms.DateInput(attrs={'type': 'datetime-local'})
-        }
+from blog.models import Comment, Post, User
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('text',)
+        fields = ("text",)
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ("author",)
+        widgets = {
+            "pub_date": forms.DateTimeInput(attrs={"type": "datetime-local"})
+        }
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
